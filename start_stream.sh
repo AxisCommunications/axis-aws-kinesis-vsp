3c73440c56a64771279a83a7e458044b42b07b3f
Dávid Molnár <130485234+david-molnar-oculai@users.noreply.github.com>
2024-12-11 14:23:51 +0100

    Handle SIGSEGV by restarting streaming process (#33)
    * Disable gstreamer fault handler and let streaming process to die
    * Restart streaming process on failure

--- start_stream.sh
+++ start_stream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gst-launch-1.0 rtspsrc \
+gst-launch-1.0 --no-fault rtspsrc \
         location="rtsp://$DEVICE_USERNAME:$DEVICE_PASSWORD@$DEVICE_IP/axis-media/media.amp" short-header=TRUE ! rtph264depay ! h264parse ! video/x-h264 !kvssink stream-name="$AWS_KINESIS_STREAM_NAME" storage-size=512 \
         aws-region="$AWS_REGION"
